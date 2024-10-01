8d84bf897b5c07a7fda9e27f59258577b2621f21
小布の微风 <109401238+SSS6r6r6SSS@users.noreply.github.com>
2024-10-01 15:34:54 +0800

    Update Message.vue

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -40,8 +40,8 @@ const store = mainStore();
 const siteLogo = import.meta.env.VITE_SITE_MAIN_LOGO;
 // 站点链接
 const siteUrl = computed(() => {
-  const url = import.meta.env.VITE_SITE_URL;
-  if (!url) return "imsyy.top".split(".");
+  const url = import.meta.env.VITE_SITE_UR;
+  if (!url) return "xiaobuawa.top".split(".");
   // 判断协议前缀
   if (url.startsWith("http://") || url.startsWith("https://")) {
     const urlFormat = url.replace(/^(https?:\/\/)/, "");
