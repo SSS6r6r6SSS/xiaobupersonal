a944b9e1b6f39e6e2545e95c448b7f5e49c19ff7
小布の微风 <109401238+SSS6r6r6SSS@users.noreply.github.com>
2024-10-01 15:33:06 +0800

    Update Right.vue

--- src/views/Main/Right.vue
+++ src/views/Main/Right.vue
@@ -20,7 +20,7 @@ const store = mainStore();
 
 // 站点链接
 const siteUrl = computed(() => {
-  const url = import.meta.env.VITE_SITE_NAME;
+  const url = import.meta.env.VITE_SITE_URL;
   if (!url) return "xiaobuawa.top".split(".");
   // 判断协议前缀
   if (url.startsWith("http://") || url.startsWith("https://")) {
