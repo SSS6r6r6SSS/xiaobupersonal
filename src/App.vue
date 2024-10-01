14eff5333690e4baec73f059bf12b2b36fc9b552
小布の微风 <109401238+SSS6r6r6SSS@users.noreply.github.com>
2024-10-01 14:40:59 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -109,15 +109,9 @@ onMounted(() => {
   const styleTitle1 = "font-size: 20px;font-weight: 600;color: rgb(244,167,89);";
   const styleTitle2 = "font-size:12px;color: rgb(244,167,89);";
   const styleContent = "color: rgb(30,152,255);";
-  const title1 = "無名の主页";
-  const title2 = `
- _____ __  __  _______     ____     __
-|_   _|  \\/  |/ ____\\ \\   / /\\ \\   / /
-  | | | \\  / | (___  \\ \\_/ /  \\ \\_/ /
-  | | | |\\/| |\\___ \\  \\   /    \\   /
- _| |_| |  | |____) |  | |      | |
-|_____|_|  |_|_____/   |_|      |_|`;
-  const content = `\n\n版本: ${config.version}\n主页: ${config.home}\nGithub: ${config.github}`;
+  const title1 = "小布の主页";
+  const title2 = "绕过江山错落 才发现你是人间星火";
+  const content = `\n\n版本: ${config.version}\n主页: xiaobuawa.top \nGithub: https://github.com/SSS6r6r6SSS`;
   console.info(`%c${title1} %c${title2} %c${content}`, styleTitle1, styleTitle2, styleContent);
 });
 
