8761d67f138d837d70c5df6e26bfa598f16929d6
小布の微风 <109401238+SSS6r6r6SSS@users.noreply.github.com>
2025-03-01 22:27:37 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Cpanel, Cloud, BookOpen, CommentAltRegular, Globe, Desktop, LaptopCode, link , Chalkboard , FileAltRegular , Wifi , Signal , FileImage } from "@vicons/fa"; // 注意使用正确的类别
+import { Cpanel, Cloud, BookOpen, CommentAltRegular, Globe, Desktop, LaptopCode, link , Chalkboard , FileAltRegular , Wifi , Signal , FileImage , Icons} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -75,6 +75,7 @@ const siteIcon = {
   Signal,
   Wifi,
   FileImage,
+  Icons,
 };
 
 // 链接跳转
